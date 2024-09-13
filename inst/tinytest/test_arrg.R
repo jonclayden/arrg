5ddc9b24145fa223f58125e56168b7c2ac67f2cd
Jon Clayden <code@clayden.org>
2024-09-13 13:24:53 +0100

    Add real tests

--- inst/tinytest/test_arrg.R
+++ inst/tinytest/test_arrg.R
@@ -1,4 +1,26 @@
+args <- arrg("test",
+             opt("h|help", "Display this usage information and exit"),
+             opt("n|times", "Run test the specifed number of times", arg="count", default=1L),
+             opt("t|time", "Print the overall run-time once the test is completed"),
+             opt("install", "Install the code before testing it"),
+             patterns=list(pat("path?", options="n,t,install"),
+                           pat("command", "arg...?", options="nt"),
+                           pat(options="h!")),
+             header="Test your code",
+             footer="Run the test on the code at the specified path (default \".\"), or run a specific command.")
 
-# Placeholder with simple test
-expect_equal(1 + 1, 2)
+expect_stdout(args$show(), "usage")
 
+p1 <- args$parse("-h")
+p2 <- args$parse(c("-tn3", "--install", "."))
+p3 <- args$parse(c("-t", "mycommand", "one", "two"))
+
+expect_true(p1$help)
+expect_null(p2$help)
+expect_true(p2$time)
+expect_equal(p2$times, 3L)
+expect_true(p2$install)
+expect_equal(p2$path, ".")
+expect_true(p3$time)
+expect_equal(p3$command, "mycommand")
+expect_equal(p3$arg, c("one","two"))
